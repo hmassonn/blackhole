c048125c4fd8274275b916ed9e596f28277ad37a
hmassonn <ogre181@hotmail.com>
2017-04-17 16:17:50 +0200

    Update brew_restore.sh

--- brew_restore.sh
+++ brew_restore.sh
@@ -1,3 +1,5 @@
 zsh -c "$(curl -fsSL https://raw.github.com/kube/42homebrew/master/install.sh)"
 zsh
 brew install valgrind
+// patch e3 sierra => https://bugsfiles.kde.org/attachment.cgi?id=104964
+// doc => https://git-scm.com/docs/git-apply
